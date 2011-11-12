a94e247f702aaf92f630433aacdb2b69362cc69f
ajlopez <webmaster@ajlopez.com>
2011-11-12 17:50:18 -0300

    First, rest tests

--- AjLisp.rb
+++ AjLisp.rb
@@ -2,6 +2,11 @@
 class List
 	attr_accessor :first
 	attr_accessor :rest
+	
+	def initialize(first=nil, rest=nil)
+		@first = first
+		@rest = rest
+	end
 end
 
 require 'test/unit'
@@ -20,5 +25,20 @@ class ListTest < Test::Unit::TestCase
 	def test_rest_is_nil
 		list = List.new
 		assert_nil(list.rest)
+	end
+	
+	def test_create_with_first
+		list = List.new("foo")
+		assert_equal("foo", list.first)
+		assert_nil(list.rest)
+	end
+	
+	def test_create_with_first_and_rest
+		rest = List.new("bar")
+		list = List.new("foo", list)
+		assert_equal("foo", list.first)
+		assert_not_nil(list.rest)
+		assert_equal("bar", list.rest)
+		assert_nil(list.rest.first)
 	end
 end
