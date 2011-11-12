85b4aa80b6ddbebd7ec4d7be4fb9c9043050d4b2
ajlopez <webmaster@ajlopez.com>
2011-11-12 18:07:16 -0300

    NamedAtom first test

--- AjLisp.rb
+++ AjLisp.rb
@@ -1,11 +1,19 @@
 
 class List
-	attr_accessor :first
-	attr_accessor :rest
+	attr_reader :first
+	attr_reader :rest
 	
 	def initialize(first=nil, rest=nil)
 		@first = first
 		@rest = rest
+	end
+end
+
+class NamedAtom
+	attr_reader :name
+	
+	def initialize(name)
+		@name = name
 	end
 end
 
@@ -35,10 +43,17 @@ class ListTest < Test::Unit::TestCase
 	
 	def test_create_with_first_and_rest
 		rest = List.new("bar")
-		list = List.new("foo", list)
+		list = List.new("foo", rest)
 		assert_equal("foo", list.first)
 		assert_not_nil(list.rest)
-		assert_equal("bar", list.rest)
-		assert_nil(list.rest.first)
+		assert_equal("bar", list.rest.first)
+		assert_nil(list.rest.rest)
+	end
+end
+
+class NamedAtomTest < Test::Unit::TestCase
+	def test_initialize
+		atom = NamedAtom.new("a")
+		assert_equal("a", atom.name)
 	end
 end
