a21b97de1dce38394ab51a6263de32c9eb556dd2
ajlopez <webmaster@ajlopez.com>
2011-12-12 06:16:12 -0300

    Change context tests to use symbols instead of strings for names

--- test/test_context.rb
+++ test/test_context.rb
@@ -10,28 +10,28 @@ class TestContext < Test::Unit::TestCase
 	
 	def test_not_defined_is_nil
 		context = AjLisp::Context.new
-		assert_nil(context.getValue("foo"))
+		assert_nil(context.getValue(:foo))
 	end
 	
 	def test_set_and_get_value
 		context = AjLisp::Context.new
-		context.setValue("foo", "bar")
-		assert_equal("bar", context.getValue("foo"))
+		context.setValue(:foo, "bar")
+		assert_equal("bar", context.getValue(:foo))
 	end
 	
 	def test_get_value_from_parent
 		parent = AjLisp::Context.new
-		parent.setValue("foo", "bar")
+		parent.setValue(:foo, "bar")
 		context = AjLisp::Context.new(parent)
-		assert_equal("bar", context.getValue("foo"))
+		assert_equal("bar", context.getValue(:foo))
 	end
 	
 	def test_override_value_from_parent
 		parent = AjLisp::Context.new
-		parent.setValue("foo", "bar")
+		parent.setValue(:foo, "bar")
 		context = AjLisp::Context.new(parent)
-		context.setValue("foo", "bar2")
-		assert_equal("bar2", context.getValue("foo"))
-		assert_equal("bar", parent.getValue("foo"))
+		context.setValue(:foo, "bar2")
+		assert_equal("bar2", context.getValue(:foo))
+		assert_equal("bar", parent.getValue(:foo))
 	end
 end
