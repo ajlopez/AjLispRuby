f85add37c26a1cbc7446c24a1dd0986e39a039aa
ajlopez <webmaster@ajlopez.com>
2016-02-08 11:33:21 -0300

    More lexer get atom rspec

--- spec/lexer_spec.rb
+++ spec/lexer_spec.rb
@@ -10,7 +10,37 @@ module AjLisp
                 token = lexer.nextToken
 
                 expect(token).to_not be_nil
+                expect(token.value).to eq("atom")
                 expect(token.type).to eq(TokenType::ATOM)
+                expect(lexer.nextToken).to be_nil
+            end
+
+            it "get atom token with spaces" do
+                source = StringSource.new "  atom   "
+                lexer = Lexer.new source
+                token = lexer.nextToken
+
+                expect(token).to_not be_nil
+                expect(token.value).to eq("atom")
+                expect(token.type).to eq(TokenType::ATOM)
+                expect(lexer.nextToken).to be_nil
+            end
+
+            it "get two atoms" do
+                source = StringSource.new "foo bar"
+                lexer = Lexer.new source
+                token = lexer.nextToken
+
+                expect(token).to_not be_nil
+                expect(token.value).to eq("foo")
+                expect(token.type).to eq(TokenType::ATOM)
+                
+                token = lexer.nextToken
+
+                expect(token).to_not be_nil
+                expect(token.value).to eq("bar")
+                expect(token.type).to eq(TokenType::ATOM)
+
                 expect(lexer.nextToken).to be_nil
             end
         end
